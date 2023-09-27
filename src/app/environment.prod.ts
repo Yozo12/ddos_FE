c4ec8be7908d022fdfcb60650a416179b70aa3d9
Matteo Milano <matteo.milano@medas-solutions.it>
2023-09-27 10:05:04 +0200

    environment

---
+++ src/app/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  link:'http://51.77.200.146',
+  name:'prod environment'
+};
---
+++ src/app/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  link:'http://127.0.0.1',
+  name:'local environment'
+};
